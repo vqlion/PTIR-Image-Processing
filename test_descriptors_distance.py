29ba4bb0b461a7bed6d28b1e578e85c8033f24a7
vql <valentinjossic@outlook.fr>
2023-04-28 15:03:28 +0200

    Initial commit

---
+++ test_descriptors_distance.py
@@ -0,0 +1,76 @@
+
+import numpy as np
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Keypoints distance computing script')
+
+parser.add_argument(
+    '--origin_image_file', type=str, required=True,
+    help='path to a file containing the keypoints and descriptors of the first image'
+)
+
+parser.add_argument(
+    '--destination_image_file', type=str, required=True,
+    help='path to a file containing the keypoints and descriptors of the second image'
+)
+
+parser.add_argument(
+    '--matrix_file', type=str, required=True,
+    help='path to a file containing the transform matrix from the origin image to the destination image'
+)
+
+parser.add_argument(
+    '--threshold', type=str, default=1,
+    help='the maximum distance between two descriptors to be considered as a pair'
+)
+
+args = parser.parse_args()
+
+def distance(desc1, desc2):
+    sum = 0
+    for i in range(len(desc1)):
+        sum += abs(desc1[i] - desc2[i])
+    return sum
+
+k = 2
+threshold = args.threshold
+
+desc1 = np.load(args.origin_image_file)['descriptors']
+desc2 = np.load(args.destination_image_file)['descriptors']
+pts1 = np.load(args.origin_image_file)['keypoints']
+pts2 = np.load(args.destination_image_file)['keypoints']
+transform_matrix = np.loadtxt(args.matrix_file)
+
+# On récupère les k descripteurs de l'image 2 les plus proches de chaque descripteur de l'image 1
+best_dist = []
+for index1 in range(len(desc1)):
+    sorted_dist = []
+    distances_list = []
+    for index2 in range(len(desc2)):
+        distances_list.append([index2, distance(desc1[index1], desc2[index2])])
+    dist = np.array(distances_list)
+    sorted_indices = np.argsort(dist[:, 1])
+    sorted_dist = dist[sorted_indices]
+    best_dist.append(sorted_dist[:k]) # On garde les k descripteurs les plus proches
+
+
+# Pour chaque point de l'image 1 on regarde s'il correspond à un des k points de l'image 2 dont on a conservé les indices
+correct = 0
+booleans = [0] * len(pts1)
+for i in range(len(pts1)):
+
+    # On applique la transformation au point de l'image 1
+    p = pts1[i]
+    p = np.append(p, 1)
+    new_p = np.matmul(p, transform_matrix)
+    new_p = new_p[:-1]
+
+    # On regarde si le point correspond à un des k points de l'image 2 retenus
+    for p2 in best_dist[i]:
+        index = int(p2[0])
+        if np.linalg.norm(new_p - pts2[index]) <= threshold:
+            correct += 1
+            break
+
+print("Correct rate: " + str(round(correct/len(pts1)*100, 2)) + "%")
---
+++ test_keypoints_distance.py
@@ -0,0 +1,66 @@
+import numpy as np  
+import argparse
+
+parser = argparse.ArgumentParser(description='Keypoints distance computing script')
+
+parser.add_argument(
+    '--origin_image_file', type=str, required=True,
+    help='path to a file containing the keypoints and descriptors of the first image'
+)
+
+parser.add_argument(
+    '--destination_image_file', type=str, required=True,
+    help='path to a file containing the keypoints and descriptors of the second image'
+)
+
+parser.add_argument(
+    '--matrix_file', type=str, required=True,
+    help='path to a file containing the transform matrix from the origin image to the destination image'
+)
+
+parser.add_argument(
+    '--threshold', type=str, default=1,
+    help='the maximum distance between two points to be considered as a pair'
+)
+
+args = parser.parse_args()
+
+threshold = args.threshold
+
+origin_arrays = np.load(args.origin_image_file)
+origin_keypoints = origin_arrays['keypoints']
+
+destination_arrays = np.load(args.destination_image_file)
+destination_keypoints = destination_arrays['keypoints']
+
+transform_matrix = np.loadtxt(args.matrix_file)
+
+origin_keypoints_transform = np.hstack(np.array([0,0]))
+keypoints_distances = np.hstack(np.array([0 for _ in range(destination_keypoints.shape[0])]))
+
+print("Counting keypoints pairs between", args.origin_image_file, "and", args.destination_image_file)
+print("Threshold is set to", args.threshold)
+print(f"{origin_keypoints.shape[0]} keypoints in {args.origin_image_file}")
+print(f"{destination_keypoints.shape[0]} keypoints in {args.destination_image_file}")
+print("----")
+
+for og_keypoint in origin_keypoints:
+    og_keypoint = np.append(og_keypoint, 1)
+    keypoint_transform = np.matmul(og_keypoint, transform_matrix)
+    keypoint_transform = keypoint_transform[:-1]
+
+    keypoint_distance_vector = np.empty(0)
+    for dst_keypoint in destination_keypoints:
+        keypoint_distance_vector = np.append(keypoint_distance_vector, np.linalg.norm(keypoint_transform-dst_keypoint))
+
+    keypoints_distances = np.vstack((keypoints_distances, keypoint_distance_vector))
+    origin_keypoints_transform = np.vstack((origin_keypoints_transform, keypoint_transform))
+
+keypoints_distances = np.delete(keypoints_distances, 0, 0)
+valid_pairs_count = 0
+
+for distances in keypoints_distances:
+    if np.amin(distances) <= threshold: 
+        valid_pairs_count += 1
+
+print("Done! Found", valid_pairs_count, "valid pairs")
