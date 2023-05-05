00bf89e35bb935885ad0a10203a94c3a15477774
IgrecL <ngowg5p3v@relay.firefox.com>
2023-05-05 14:39:53 +0200

    Graphs for comparison

--- results_plotter.py
+++ results_plotter.py
@@ -2,36 +2,49 @@ import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 import json
+import glob
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    '--dataset', type=int, required=True,
+    '--dataset', type=str, required=True,
     help='id of the dataset you want to display'
+)
+
+parser.add_argument(
+    '--folder', type=str, required=True,
+    help='folder containing the files keypoints and descriptors'
 )
 
 args = parser.parse_args()
 
+files_path = glob.glob(f"{args.folder}/*.json")
+if len(files_path) <= 1:
+    raise Exception('Need at least two models for comparison.')
 json_files = []
-with open('message5.json', 'r') as file:
-    json = json.load(file)
-    json_files.append(json)
-    json_files.append(json)
-    json_files.append(json)
+for path in files_path:
+    with open(path, 'r') as file:
+        json_files.append(json.load(file))
+
+indices = []
+for model in range(len(json_files)):
+    for i in range(len(json_files[model])):
+        if args.dataset in json_files[model][i]["image_folder"].split("\\")[-1].split("/")[-1]:
+            indices.append(i) 
+            break
+
+print(indices)
 
 # Titre du graphique
 fig, axs = plt.subplots(3, len(json_files), figsize=(18, 10))
-fig.suptitle(json_files[0][args.dataset]["image_folder"], fontsize=25)
+fig.suptitle(json_files[0][indices[0]]["image_folder"], fontsize=25)
 plt.subplots_adjust(hspace=0.3)
 
 # Pour le dataset choisi, on crée un graph pour chaque modèle et on trace une courbe dessus pour chacune des comparaisons des images
 for model in range(len(json_files)):
-    dataset = json_files[model][args.dataset]
+    dataset = json_files[model][indices[model]]
     for j in range(len(dataset["images_tests"])):
-        for i in range(3):
-            axs[i][model].set_xlabel('Threshold')
-            axs[i][model].grid(True)
-
+        axs[0][model].grid(True)
         axs[0][model].set_xlim(0, 50)
         axs[0][model].set_ylim(0, 100)
         axs[0][model].set_xlabel('Threshold')
@@ -39,12 +52,14 @@ for model in range(len(json_files)):
         axs[0][model].plot(dataset["images_tests"][j]["keypoints_distances"])
         axs[0][model].set_ylabel('Keypoints success rate')
 
+        axs[1][model].grid(True)
         axs[1][model].set_xlim(0, 50)
         axs[1][model].set_ylim(0, 100)
         axs[1][model].set_xlabel('Threshold')
         axs[1][model].plot(dataset["images_tests"][j]["descriptors_distances"])
         axs[1][model].set_ylabel('Descriptors success rate')
 
+        axs[2][model].grid(True)
         axs[2][model].set_ylabel('Scores regressions')
         axs[2][model].set_xlabel('Keypoints')
         for k in np.arange(0, len(dataset["scores"]), 3):
